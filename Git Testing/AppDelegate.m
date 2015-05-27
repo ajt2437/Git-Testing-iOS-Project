cdb3297d3b6ef90165c628b6cad696f225d5255d
Abelardo Torres <Vicky@Abelardos-Mac-mini.local>
2015-05-27 14:35:43 -0500

    still testing

--- Git Testing/AppDelegate.m
+++ Git Testing/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"Nothing new here");
+    NSLog(@"Woop did something wrong");
     return YES;
 }
 
