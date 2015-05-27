d6164b993bb1adfee3fce75db9296972e8ed93fb
Abelardo Torres <Vicky@Abelardos-Mac-mini.local>
2015-05-27 13:52:11 -0500

    Initial Commit

---
+++ Git Testing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Testing
+//
+//  Created by Abelardo Torres on 5/27/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Testing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Git Testing
+//
+//  Created by Abelardo Torres on 5/27/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
