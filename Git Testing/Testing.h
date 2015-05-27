1d669c57d208150a15ccf48928aae0a80d905eb0
Abelardo Torres <Vicky@Abelardos-Mac-mini.local>
2015-05-27 15:59:14 -0500

    add testing class

---
+++ Git Testing/Testing.h
@@ -0,0 +1,13 @@
+//
+//  Testing.h
+//  Git Testing
+//
+//  Created by Abelardo Torres on 5/27/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Testing : NSObject
+
+@end
---
+++ Git Testing/Testing.m
@@ -0,0 +1,13 @@
+//
+//  Testing.m
+//  Git Testing
+//
+//  Created by Abelardo Torres on 5/27/15.
+//  Copyright (c) 2015 AE Development. All rights reserved.
+//
+
+#import "Testing.h"
+
+@implementation Testing
+
+@end
